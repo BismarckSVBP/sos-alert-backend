1e9e9a5102c43a7baf8f19f1d56e0aa022b45f31
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-07-19 01:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ const __dirname = path.resolve();
 
 // ✅ CORS Configuration for Render (API) + Vercel (Frontend)
 const allowedOrigins = [
+  "https://sos-web-pied.vercel.app",
   "https://sos-alert-beta.vercel.app",       // ✅ Frontend (Vercel production)
   "https://sos-alert-beta.onrender.com",     // ✅ If frontend is also deployed on Render
   "http://localhost:3000",                   // ✅ Local dev
